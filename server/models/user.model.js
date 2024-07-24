5a3fa6b4c0a6ead006b035360b32d15436f396b0
Ryan Naini <ryan.naini29@gmail.com>
2024-07-23 21:15:36 -0400

    feat: create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, 'Please enter a fullname'],
+    },
+    username: {
+      type: String,
+      required: [true, 'Please enter a username'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter a password'],
+      minlength: [6, 'Password must be at least 6 characters'],
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    grade: {
+      type: Number,
+      required: [true, 'Please enter a grade'],
+    },
+    school: {
+      type: Object,
+      required: [true, 'Please enter a school'],
+    },
+    schedule: {
+      type: Object,
+      default: {},
+    },
+    interests: {
+      type: Array,
+      required: [true, 'Please select at least 1 club or sport interest'],
+    },
+    contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }], // list of users that the user has stored to have conversations with
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+
+  next();
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
