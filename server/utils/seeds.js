b6844ddcfc8a0c347aeccd665992ab3f6b1733d0
Ryan Naini <ryan.naini29@gmail.com>
2024-07-26 15:11:53 -0400

    chore: update the fake user passwords to be the same for testing purposes

--- server/utils/seeds.js
+++ server/utils/seeds.js
@@ -46,7 +46,7 @@ const generateFakeUsers = async () => {
       const fakeUser = {
         fullName: firstName + ' ' + lastName,
         username: faker.internet.displayName(),
-        password: faker.internet.password(),
+        password: process.env.FAKE_USER_PASSWORD,
         profilePicture: `https://avatar.iran.liara.run/public/?username=${firstName}`,
         school: schoolInfo,
         grade: generateGrade(i),
