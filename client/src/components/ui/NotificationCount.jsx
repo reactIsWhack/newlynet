c4d982cfa0632a7b14cf2d735d5c21f58d01c81b
Ryan Naini <ryan.naini29@gmail.com>
2024-08-15 19:39:53 -0400

    refactor: render the number of unread club chats on club server icon

--- client/src/components/ui/NotificationCount.jsx
+++ client/src/components/ui/NotificationCount.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../app/features/user/userSlice';
 import calculateUnreadMsgCount from '../../utils/calculateUnreadMsgCount';
+import { selectClubChat } from '../../app/features/clubChat/clubChatSlice';
 
 const NotificationCount = ({ array, includeInvites }) => {
-  const { serverInvites } = useSelector(selectUser);
-  const count = calculateUnreadMsgCount(array);
-  console.log(count + serverInvites.length);
+  const { serverInvites, unreadClubChats } = useSelector(selectUser);
+  const { chats } = useSelector(selectClubChat);
+  const unreadServers = unreadClubChats.filter(
+    (server) => !chats.some((chat) => chat._id === server.chat._id)
+  );
+
+  const count = includeInvites
+    ? unreadServers.length
+    : calculateUnreadMsgCount(array);
 
   return (
     <div className="w-6 h-6 rounded-full bg-red-500 absolute flex items-center justify-center text-white text-base -top-2 -right-2">
